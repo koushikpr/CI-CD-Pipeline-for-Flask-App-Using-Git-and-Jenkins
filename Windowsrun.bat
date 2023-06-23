5f96b6a18cea091e0e467016faf1eb480f7bb8be
koushikpr <karanboltz@gmail.com>
2023-06-23 22:54:10 +0530

    Windows Batch File for Jenkins Job

---
+++ Windowsrun.bat
@@ -0,0 +1,9 @@
+mkdir testing
+cd testing
+git pull https://github.com/koushikpr/CI-CD-Pipeline-for-Flask-App-Using-Git-and-Jenkins
+
+cd Terraform
+
+terraform init
+terraform plan 
+terraform apply
